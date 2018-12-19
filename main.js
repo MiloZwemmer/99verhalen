a391908f44a7526c66861ea274e6dba54dc851ef
MiloZwemmer <45543551+MiloZwemmer@users.noreply.github.com>
2018-12-19 11:47:05 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -4,16 +4,16 @@ function myFunction() {
 }
 
 /* sluit de dropdown als de gebruiker naast het menuveld klikt */
-window.onclick = function(event) {
-  if (!event.target.matches('.dropdownButton')) {
+window.onclick = function (event) {
+    if (!event.target.matches('.dropdownButton')) {
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
     }
-  }
 }
